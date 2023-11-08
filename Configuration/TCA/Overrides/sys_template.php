40dd0bd9bd0cf3306a20a01a163a15040cbc35e0
Andy Wesely <mail@andy-wesely.de>
2023-11-08 10:41:51 +0100

    Fix use of TYPO3_MODE constant

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -9,7 +9,7 @@ declare(strict_types=1);
  * LICENSE.txt file that was distributed with this source code.
  */
 
-defined('TYPO3_MODE') || die('Access denied.');
+defined('TYPO3') || die('Access denied.');
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
     'typo3_pagerfanta',
